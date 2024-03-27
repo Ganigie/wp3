fcb2a6d1a58e7001c81c0eefd51a767b133fb557
Ganigie <ganiabdul691@gmail.com>
2024-03-26 20:09:01 -0800

    tugas pertemuan 1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya gani
+        echo"Nama saya muhamad abdul ghani
             Saya tingga di daerah Ciputat
             olah raga yang saya sukai adalah
             Bulutangkis";
